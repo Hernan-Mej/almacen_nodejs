b0a2cde5dca91c33ca376fa53d5f30c3c8ea8a78
Hernan Mej <hdmejia@uniguajira.edu.co>
2024-04-24 11:20:49 -0500

    minor fix

--- src/models/productoVentas.ts
+++ src/models/productoVentas.ts
@@ -4,15 +4,33 @@ import { database } from "../database/db";
 export class ProductoVentas extends Model {
     public productoId!: number;
     public ventaId!: number;
+    public cantidad!: string;
+    public precio!: string;
+    public total!: string;
 }
 
 export interface ProductoVentasI {
     productoId: number;
     ventaId: number;
+    cantidad: string;
+    precio: string;
+    total: string;
 }
 
 ProductoVentas.init(
     {
+        cantidad: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        precio: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        total: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
     }, {
     tableName: "productoVentas",
     sequelize: database,
